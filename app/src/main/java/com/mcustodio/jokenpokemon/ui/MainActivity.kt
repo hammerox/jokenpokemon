95b879d8f96112acc0011b93a676f9e074db4efd
Mauricio Custodio <mcustodio@stone.com.br>
2018-12-18 23:43:42 -0200

    Implemented MainActivity.

--- app/src/main/java/com/mcustodio/jokenpokemon/ui/MainActivity.kt
+++ app/src/main/java/com/mcustodio/jokenpokemon/ui/MainActivity.kt
@@ -1,13 +1,34 @@
 package com.mcustodio.jokenpokemon.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mcustodio.jokenpokemon.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_main_play.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
+
+        button_main_ranking.setOnClickListener {
+            val intent = Intent(this, RankingActivity::class.java)
+            startActivity(intent)
+        }
+
+        button_main_about.setOnClickListener {
+            val intent = Intent(this, AboutActivity::class.java)
+            startActivity(intent)
+        }
+
+        button_main_leave.setOnClickListener {
+            finishAndRemoveTask()
+        }
     }
 }
