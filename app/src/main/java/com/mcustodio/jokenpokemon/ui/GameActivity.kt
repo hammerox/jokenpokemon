d93cdb30d5fcafa30d850424b32b9e17128baad1
Mauricio Custodio <mcustodio@stone.com.br>
2018-12-19 00:25:22 -0200

    Implemented GameActivity.

--- app/src/main/java/com/mcustodio/jokenpokemon/ui/GameActivity.kt
+++ app/src/main/java/com/mcustodio/jokenpokemon/ui/GameActivity.kt
@@ -1,13 +1,71 @@
 package com.mcustodio.jokenpokemon.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import com.mcustodio.jokenpokemon.R
+import kotlinx.android.synthetic.main.activity_game.*
+import kotlin.random.Random
+
+
 
 class GameActivity : AppCompatActivity() {
+
+    enum class Element(val imageRes: Int) {
+        Grass(R.drawable.bulbasaur),
+        Fire(R.drawable.charmander),
+        Water(R.drawable.squirtle);
+
+        fun winsAgainst(): Element = when(this) {
+            Grass -> Water
+            Fire -> Grass
+            Water -> Fire
+        }
+    }
+
+    private var score = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        image_game_grass.setOnClickListener { startGame(Element.Grass) }
+        image_game_fire.setOnClickListener { startGame(Element.Fire) }
+        image_game_water.setOnClickListener { startGame(Element.Water) }
+    }
+
+    private fun startGame(userChoice: Element) {
+        image_game_yourchoice.setImageDrawable(resources.getDrawable(userChoice.imageRes, applicationContext.theme))
+
+        val randomIndex = Random.nextInt(0,2)
+        val enemyChoice = Element.values().get(randomIndex)
+        image_game_enemychoice.setImageDrawable(resources.getDrawable(enemyChoice.imageRes, applicationContext.theme))
+
+        when {
+            userChoice.winsAgainst() == enemyChoice -> {
+                score += 2
+                text_game_result.text = "VITÓRIA: +2 pontos"
+            }
+            userChoice == enemyChoice -> {
+                score += 1
+                text_game_result.text = "EMPATE: +1 ponto"
+            }
+            else -> {
+                linear_game_options.visibility = View.INVISIBLE
+                text_game_result.text = "GAME OVER"
+                showGameOverScreen()
+            }
+        }
+    }
+
+    private fun showGameOverScreen() {
+        Handler().postDelayed({
+            val intent = Intent(this, GameOverActivity::class.java)
+            intent.putExtra("score", score)
+            startActivity(intent)
+        },1000L)
     }
 }
