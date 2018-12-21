52569c891ec0f2f1b3f4ef5a2aca73e0ff0cf624
Mauricio Custodio <mcustodio@stone.com.br>
2018-12-21 05:07:50 -0200

    Implemented AboutActivity.

--- app/src/main/java/com/mcustodio/jokenpokemon/ui/AboutActivity.kt
+++ app/src/main/java/com/mcustodio/jokenpokemon/ui/AboutActivity.kt
@@ -2,12 +2,15 @@ package com.mcustodio.jokenpokemon.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mcustodio.jokenpokemon.BuildConfig
 import com.mcustodio.jokenpokemon.R
+import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+        text_about_version.text = BuildConfig.VERSION_NAME
     }
 }
